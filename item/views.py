a2370536e21c23882be72e492cfcbb6280a1cd94
CHETAN GARG <100898956+ChetanGarg6842@users.noreply.github.com>
2023-03-03 14:49:04 +0530

    improving view items

--- item/views.py
+++ item/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Item
 
 # Create your views here.
+def detail(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+
+    return render(request, 'item/detail.html',{
+        'item':item
+    })
