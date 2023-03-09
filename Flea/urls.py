d789faafd9538221c067fd6d90cf74e4a28e69cd
Garv <garvy312@gmail.com>
2023-03-09 19:47:17 +0530

    Futher Progress in the Item Section

--- Flea/urls.py
+++ Flea/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -7,7 +7,7 @@ from main.views import index, contact
 
 urlpatterns = [
     path('', index, name='index'),
+    path('items/', include('item.urls')),
     path('contact/', contact, name='contact'),
-    # path('about/', about, name='about'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
