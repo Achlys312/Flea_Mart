eac2af7d90a46c08d8258b2741c33d975c199b74
Garv <garvy312@gmail.com>
2023-03-11 10:22:31 +0530

    optimizing the code before creation of signup page

--- Flea/urls.py
+++ Flea/urls.py
@@ -3,11 +3,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from main.views import index, contact
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', include('main.urls')),
     path('items/', include('item.urls')),
-    path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/urls.py
+++ main/urls.py
@@ -1,3 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'main'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('contact/', views.contact, name='contact'),
+]
