446d11c2a6da44c5c1867420283c6a23c5ce5565
Garv <garvy312@gmail.com>
2023-03-23 00:37:45 +0530

    correction

--- main/urls.py
+++ main/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('contact/', views.contact, name='contact'),
     path('signup/', views.signup, name='signup'),
-    path('login/', auth_views.LoginView.as_view(authentication_form=LoginForm), name='login')
+    path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm), name='login')
 ]
