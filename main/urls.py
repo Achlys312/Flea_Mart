783ff7647dabc9ebb907990eadca8f3c65174685
Garv <garvy312@gmail.com>
2023-03-23 00:36:19 +0530

    Correcting path of login in base.html

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from .forms import LoginForm
 
 app_name = 'main'
 
@@ -8,5 +9,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('contact/', views.contact, name='contact'),
     path('signup/', views.signup, name='signup'),
-    path('login/', auth_views.LoginView.as_view(), name='login')
+    path('login/', auth_views.LoginView.as_view(authentication_form=LoginForm), name='login')
 ]
