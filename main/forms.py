8b996dc107d40aa0d3cc8aab5b2b3f08ebf39593
Garv <garvy312@gmail.com>
2023-03-23 00:32:06 +0530

    Starting of Login Page

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,16 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': "Your Username",
+        "class": "w-full py4 px-6 rounded-xl"
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': "Your Password",
+        "class": "w-full py4 px-6 rounded-xl"
+    }))
 
 class SignUpForm(UserCreationForm):
     class meta:
