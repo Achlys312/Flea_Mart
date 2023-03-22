fcc02de506dd8d06ac5425c194cc86d40aeacfd4
Garv <garvy312@gmail.com>
2023-03-23 00:33:54 +0530

    Adding path of login in urls

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = 'main'
 
@@ -7,4 +8,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('contact/', views.contact, name='contact'),
     path('signup/', views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(), name='login')
 ]
