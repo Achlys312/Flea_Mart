9769ca236c7b3414e1c61daad4a8a8f71ca04771
Kartik Dhoundiyal <kartikdhoundiyal2491@gmail.com>
2023-03-31 19:54:49 +0530

    fixing styling for the new items page

--- item/form.py
+++ item/form.py
@@ -5,3 +5,9 @@ class NewItemFormm(forms.ModelForm):
     class Meta:
         model = Item
         fields = ('category', 'name', 'description', 'price', 'image', )
+
+        widgets = {
+            'category': forms.Select(attrs={
+                'class':'w-full py-4 px-6 rounded-xl border'
+            })
+        }
