9318a74c820cf36ccfa58472cec0d46b2edac8cd
Kartik Dhoundiyal <kartikdhoundiyal2491@gmail.com>
2023-04-03 12:53:55 +0530

    Adding structure to Dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,16 +1,11 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
-from item.models import Item
+from item .models import Item
 from django.contrib.auth.decorators import login_required
-
 
 @login_required
 def index(request):
-   items = Item.objects.filter(created_by=request.user)
+    items = Item.objects.filter(created_by=request.user)
 
-
-   return render(request, 'dashboard/index.html', {
-       'items': items
-   })
+    return render(request, 'dashboard/index.html', {
+        'items': items
+    })
