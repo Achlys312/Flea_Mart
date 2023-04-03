a4acf8b4bae075a7a16e02ee077ec3d58c41c050
Kartik Dhoundiyal <kartikdhoundiyal2491@gmail.com>
2023-04-03 13:06:09 +0530

    Adding dashboard to main

--- Flea/urls.py
+++ Flea/urls.py
@@ -3,9 +3,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
-    path('', include('main.urls')),
-    path('items/', include('item.urls')),
-    path('admin/', admin.site.urls),
+   path('', include('main.urls')),
+   path('items/', include('item.urls')),
+   path('dashboard/', include('dashboard.urls')),
+   path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
