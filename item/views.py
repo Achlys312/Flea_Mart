5611e3144660ecefe6583c39809bcfb59b3f33d6
CHETAN GARG <100898956+ChetanGarg6842@users.noreply.github.com>
2023-04-09 22:39:39 +0530

    changes in index

--- item/views.py
+++ item/views.py
@@ -31,3 +31,10 @@ def new(request):
         'form': form,
         'title': 'New Item',
     })
+
+@login_required
+def delete(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    item.delete()
+
+    return redirect('dashboard:index')
