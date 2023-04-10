1ad69d1f4f9b9c703bd13ecf74978f376381e798
CHETAN GARG <100898956+ChetanGarg6842@users.noreply.github.com>
2023-04-10 18:58:47 +0530

    adding path of browse

--- item/views.py
+++ item/views.py
@@ -4,7 +4,16 @@ from .models import Item
 from .form import NewItemFormm, EditItemForm
 
 
+
 # Create your views here.
+
+def items(request):
+    items = Item.objects.filter(is_sold=False)
+    return render(request, 'item/items.html', {
+        'items':items,
+    })
+
+
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
     related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
