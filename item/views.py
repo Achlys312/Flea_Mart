defc761540ea422dcf54d55365b9d1a94b4cfe1f
Kartik Dhoundiyal <kartikdhoundiyal2491@gmail.com>
2023-04-11 20:46:34 +0530

    optimizing the search

--- item/views.py
+++ item/views.py
@@ -1,65 +1,82 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Item
-from .form import NewItemFormm, EditItemForm 
+from .form import NewItemFormm, EditItemForm
+
 
 # Create your views here.
 
+
 def items(request):
-    items = Item.objects.filter(is_sold=False)
-    return render(request, 'item/items.html', {
-        'items':items,
-    })
+   query = request.GET.get('query', '')
+   items = Item.objects.filter(is_sold=False)
+   return render(request, 'item/items.html', {
+       'items':items,
+       'query':query,
+   })
+
+
 
 
 def detail(request, pk):
-    item = get_object_or_404(Item, pk=pk)
-    related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
+   item = get_object_or_404(Item, pk=pk)
+   related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
 
-    return render(request, 'item/detail.html',{
-        'item':item,
-        'related_items':related_items,
-    })
+
+   return render(request, 'item/detail.html',{
+       'item':item,
+       'related_items':related_items,
+   })
+
 
 @login_required
 def new(request):
-    if request.method == 'POST':
-        form = NewItemFormm(request.POST, request.FILES)
+   if request.method == 'POST':
+       form = NewItemFormm(request.POST, request.FILES)
 
-        if form.is_valid():
-            item = form.save(commit=False)
-            item.created_by = request.user
-            item.save()
-            return redirect('item:detail', pk=item.id)
-    else:
-        form = NewItemFormm()
 
-    return render(request, 'item/form.html', {
-        'form': form,
-        'title': 'New Item',
-    })
+       if form.is_valid():
+           item = form.save(commit=False)
+           item.created_by = request.user
+           item.save()
+           return redirect('item:detail', pk=item.id)
+   else:
+       form = NewItemFormm()
+
+
+   return render(request, 'item/form.html', {
+       'form': form,
+       'title': 'New Item',
+   })
+
 
 @login_required
 def edit(request, pk):
-    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+   item = get_object_or_404(Item, pk=pk, created_by=request.user)
 
-    if request.method == 'POST':
-        form = EditItemForm(request.POST, request.FILES, instance=item)
 
-        if form.is_valid():
-            form.save()
+   if request.method == 'POST':
+       form = EditItemForm(request.POST, request.FILES, instance=item)
 
-            return redirect('item:detail', pk=item.id)
-    else:
-        form = EditItemForm(instance=item)
 
-    return render(request, 'item/form.html', {
-        'form': form,
-        'title': 'Edit item',
-    })
+       if form.is_valid():
+           form.save()
+
+
+           return redirect('item:detail', pk=item.id)
+   else:
+       form = EditItemForm(instance=item)
+
+
+   return render(request, 'item/form.html', {
+       'form': form,
+       'title': 'Edit item',
+   })
+
 
 @login_required
 def delete(request, pk):
-    item = get_object_or_404(Item, pk=pk, created_by=request.user)
-    item.delete()
-    return redirect('dashboard:index')
+   item = get_object_or_404(Item, pk=pk, created_by=request.user)
+   item.delete()
+   return redirect('dashboard:index')
+
