a65171fcd256fa077a9744c1bbfbde521ec41acf
Garv312 <garvy312@gmail.com>
2023-04-11 21:06:28 +0530

    correcting the dashboard/views

--- dashboard/views.py
+++ dashboard/views.py
@@ -10,16 +10,7 @@ def index(request):
         'items': items,
     })
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
 @login_required
 def delete(request, pk):
     item = get_object_or_404(Item, pk=pk, created_by=request.user)
     item.delete()
->>>>>>> fa57db1ec9d274e17ea09239e415a61b27f2d7a7
->>>>>>> 1d2d62a68b50d4263e582a7923ad438dbfff09d8
->>>>>>> b28ccd908ac88b9bfce40504e4d132d0e41c1622
