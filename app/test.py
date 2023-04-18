f2534313940fd08504c68a546c46ba3d4dfb07fa
Garv312 <garvy312@gmail.com>
2023-04-18 10:58:13 +0530

    test

--- app/test.py
+++ app/test.py
@@ -9,14 +9,14 @@ driver = webdriver.Chrome()
 # Visit the home page of the Django app
 driver.get("http://127.0.0.1:8000/")
 
-uzu = "kadbubf12"
+uzu = "kadbubffyf12"
 
 
-time.sleep(2)
+# time.sleep(2)
 signup = driver.find_element(By.XPATH, '/html/body/nav/div/a[3]')
 signup.click()
 
-time.sleep(2)
+# time.sleep(2)
 username = driver.find_element(By.XPATH, '//*[@id="id_username"]')
 username.send_keys(uzu)
 
@@ -29,7 +29,7 @@ password.send_keys('MSIlaptop@1')
 repassword = driver.find_element(By.XPATH, '//*[@id="id_password2"]')
 repassword.send_keys('MSIlaptop@1')
 
-time.sleep(2)
+# time.sleep(2)
 submit = driver.find_element(By.XPATH, '/html/body/div/div/form/button')
 submit.click()
 
@@ -37,5 +37,5 @@ login = driver.find_element(By.XPATH, '//*[@id="id_username"]')
 login.send_keys(uzu)
 passwordd = driver.find_element(By.XPATH, '//*[@id="id_password"]')
 passwordd.send_keys("MSIlaptop@1")
-time.sleep(2)
+# time.sleep(2)
 print("Test Successfully")
