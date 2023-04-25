c99854d8f6451f09e251d1cb494ccc9834b7d166
Garv312 <garvy312@gmail.com>
2023-04-25 14:48:03 +0530

    added a logout feature

--- app/item/views.py
+++ app/item/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.shortcuts import render, get_object_or_404, redirect
-
+from django.contrib.auth import logout
 from .forms import NewItemForm, EditItemForm
 from .models import Category, Item
 
